332e2971b983114874cea900999c79a656fb046a
jandi-Alejandro <aespinozam@alumnosuls.cl>
2022-06-11 15:48:23 -0400

    Alejandro
    Se agregan las otras clases

---
+++ baseprueba/modelos/anotacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colegio_BD.Models
+{
+    internal class anotacion
+    {
+        public string _id { get; set; }
+        public string nombre_alumno { get; set; }
+        public string comentario { get; set; }
+        public string tipo { get; set; }
+        public string materia { get; set; }
+        public int fecha { get; set; }
+    }
+}
