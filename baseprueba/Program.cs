70de7c7ccfcdf7dcf10808e97561022d94d5d1f3
fernando osorio carmona <49483430+fernandooso@users.noreply.github.com>
2022-06-11 17:40:50 -0400

    se crean los metodos

--- baseprueba/Program.cs
+++ baseprueba/Program.cs
@@ -1,4 +1,4 @@
-﻿using baseprueba.conexion;
+﻿
 using baseprueba.modelos;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace baseprueba
 {
-    internal  class Program
+    internal  class Program:conection
     {
         [STAThread]
         private static void Main()
@@ -19,51 +19,20 @@ namespace baseprueba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            Console.WriteLine("hola");
-            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://mongouser:mongouser@taller2bd2.cpnky.mongodb.net/?retryWrites=true&w=majority");
-            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            var client = new MongoClient(settings);
-            var database = client.GetDatabase("TallerBD2");
-            Console.WriteLine("hola");
-            var apoderadodb = database.GetCollection<apoderado>("apoderado");
-            Console.WriteLine("hola");
-            var uwuwp = new apoderado() { 
-                direccion = "direccion prueba",
-                fecha_nacimiento="0000-00-00",
-                nombre_completo="nombre prueba apoderado",
-                rut="11111111-1",
-                telefono="11111111"};
-            Console.WriteLine("direccion:",uwuwp.direccion);
-            var alumnop = new alumno()
-            {
-                fecha_de_nacimiento = "1111-11-11",
-                nombre_alumno = "alfonso",
-                rut_alumno = "22222222-2"
-            };
-            apoderadodb.InsertOne(uwuwp);
-            Console.WriteLine("ewew");
 
-            agregarsub(database,alumnop);
+            conection unaconexion = new conection();
 
+            IMongoDatabase database=unaconexion.conexion_Mongo();
+            unaconexion.agregar_apoderado(database, "una direccion","un nombre",
+                "111","unafecha","4");
+            unaconexion.agrega_alumno(database, "unafechanac", "federico", "22", "4");
+            unaconexion.agrega_anotacion(database, "pedrito", "se hace una en clase",
+                "negativa", "lenguaje", "hoy");
+            unaconexion.agrega_profesor(database, "profesr", "unrut", "111", "matematica", "5b",
+                "desde siempre", "unacontraseña");
+            unaconexion.agrega_notas(database, "pedrito", "lenguaje", "7.0");
+            unaconexion.agrega_asistencia(database, "pepe", "si", "hoy");
         }
-
-        private static void agregarsub(IMongoDatabase database, alumno alumnop)
-        {
-            BsonDocument documentillo = alumnop.ToBsonDocument();
-            BsonDocument newSubdocument = documentillo;
-           //  newSubdocument.Remove("Id");
-            var collection = database.GetCollection<BsonDocument>("apoderado");
-            var filter = Builders<BsonDocument>.Filter.Eq("rut","11111111-1");
-            var update = Builders<BsonDocument>.Update.Push("a_cargo", newSubdocument);
-            var result = collection.UpdateOne(filter, update);
-            if (result.IsModifiedCountAvailable)
-            {
-                if (result.ModifiedCount == 1)
-                {
-                    Console.WriteLine("funciono");
-                }
-            }
-            Console.WriteLine("fallos");
-        }
+        
     }
 }
---
+++ baseprueba/modelos/conection.cs
@@ -0,0 +1,122 @@
+﻿using baseprueba.conexion;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseprueba.modelos
+{
+    internal class conection
+    {
+        
+        
+        public IMongoDatabase conexion_Mongo()
+        {
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://mongouser:mongouser@taller2bd2.cpnky.mongodb.net/?retryWrites=true&w=majority");
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("TallerBD2");
+
+            return database;
+        }
+        
+        public void agregar_apoderado(IMongoDatabase database,String direccion,
+            String nombre,String telefono, String fechanac, String rut)
+        {
+            var apoderadodb = database.GetCollection<apoderado>("apoderado");
+            var apoderadoingreso = new apoderado()
+            {
+                direccion = direccion,
+                fecha_nacimiento = fechanac,
+                nombre_completo = nombre,
+                rut = rut,
+                telefono = telefono
+            };
+
+            apoderadodb.InsertOne(apoderadoingreso);
+        }
+
+        public void agrega_alumno(IMongoDatabase database,String fnac,
+            String nomal, String rutal, String rutapoderado)
+        {
+            var alumnop = new alumno()
+            {
+                fecha_de_nacimiento = fnac,
+                nombre_alumno = nomal,
+                rut_alumno = rutal
+            };
+
+            BsonDocument documentillo = alumnop.ToBsonDocument();
+            BsonDocument newSubdocument = documentillo;
+            var collection = database.GetCollection<BsonDocument>("apoderado");
+            var filter = Builders<BsonDocument>.Filter.Eq("rut", rutapoderado);
+            var update = Builders<BsonDocument>.Update.Push("a_cargo", newSubdocument);
+            var result = collection.UpdateOne(filter, update);
+        }
+
+        public void agrega_anotacion(IMongoDatabase database,String nomal,
+            String comentario, String tipo,String materia, String fechano)
+        {
+            var anotaciondb = database.GetCollection<anotacion>("anotacion");
+            var anotacioningreso = new anotacion()
+            {
+                comentario = comentario,
+                tipo = tipo,
+                materia = materia,
+                fecha = fechano,
+                nombre_alumno = nomal
+            };
+            anotaciondb.InsertOne(anotacioningreso);
+        }
+
+        public void agrega_asistencia(IMongoDatabase database,String nombre_alumno,
+            String asistenciaa, String fecha)
+        {
+            var asistenciandb = database.GetCollection<Asistencia>("asistencia");
+            var asistenciaingreso = new Asistencia()
+            {
+                asistencia = asistenciaa,
+                fecha=fecha,
+                nombre_alumno=nombre_alumno
+            };
+            asistenciandb.InsertOne(asistenciaingreso);
+        }
+
+        public void agrega_notas(IMongoDatabase database, String nombre_alumno, String materia,
+            String nota)
+        {
+            var notasdb = database.GetCollection<notas>("notas");
+            var notaingreso = new notas()
+            {
+                materia = materia,
+                nombre_alumno = nombre_alumno,
+                nota = nota
+            };
+            notasdb.InsertOne(notaingreso);
+        }
+
+        public void agrega_profesor(IMongoDatabase database, String nombre_completo, String rut,
+            String telefono, String especialidad, String curso_jefatura, String fecha, String contraseña)
+        {
+            var profesordb = database.GetCollection<profesor>("profesor");
+            var profeingreso = new profesor()
+            {
+                fecha = fecha,
+                contraseña=contraseña,
+                curso_jefatura = curso_jefatura,
+                especialidad = especialidad,
+                nombre_completo = nombre_completo,
+                rut = rut,
+                telefono = telefono
+            };
+            profesordb.InsertOne(profeingreso);
+        }
+
+    }
+
+    
+
+}
