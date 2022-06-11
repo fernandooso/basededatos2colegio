6c822dd7898f7b44c18bf6ebc12e53371afcfc3c
jandi-Alejandro <aespinozam@alumnosuls.cl>
2022-06-11 16:28:27 -0400

    Update conexion.cs

--- baseprueba/modelos/conexion.cs
+++ baseprueba/modelos/conexion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace baseprueba.modelos
 {
     internal class conexion
     {
+
+
+        public static void conexion_Mongo()
+        {
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://mongouser:mongouser@taller2bd2.cpnky.mongodb.net/?retryWrites=true&w=majority");
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("TallerBD2");
+        }
     }
+    
 }
