d85f4e296af202637bf3c8da6a56e62ced831fb3
jandi-Alejandro <aespinozam@alumnosuls.cl>
2022-06-11 16:16:29 -0400

    Update profesor.cs

--- baseprueba/modelos/profesor.cs
+++ baseprueba/modelos/profesor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace baseprueba.modelos
 {
     internal class profesor
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string _id { get; set; }
+        [BsonElement("nombre_completo")]
+        public string nombre_completo { get; set; }
+        [BsonElement("rut")]
+        public string rut { get; set; }
+        [BsonElement("telefono")]
+        public string telefono { get; set; }
+        [BsonElement("especialidad")]
+        public string especialidad { get; set; }
+        [BsonElement("curso_jefatura")]
+        public string curso_jefatura { get; set; }
+        [BsonElement("fecha")]
+        public string fecha { get; set; }
+        [BsonElement("contraseña")]
+        public string contraseña { get; set; }
     }
 }
